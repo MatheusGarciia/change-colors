a644d9e78baa498060f8a889bc3ff88a465cdc09
Matheus Garcia <garciiamatheus98@gmail.com>
2026-04-08 16:03:46 -0300

    Hex format page

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const btn = document.getElementById('btn')
 const color = document.querySelector('.span-color')
 
 btn.addEventListener('click', function(){
-    const randomNumber = Math.floor(Math.random() * colors.length)
+    const randomNumber = getRandomNumber()
     const mainColor = document.querySelector('#main-color')
 
     mainColor.style.backgroundColor = colors[randomNumber]
     color.textContent = colors[randomNumber]
 })
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length)
+}
