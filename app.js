1b12b2e24ad1663b4a4d0957ac4db6a6ea17ecbd
Matheus Garcia <garciiamatheus98@gmail.com>
2026-04-10 17:31:10 -0300

    adjusts in texts

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025']
+const colors = ['green', 'red', 'blue', 'yellow', 'purple', 'orange', 'pink', 'brown', 'gray', 'black', 'white', 'cyan', 'magenta', 'lime', 'maroon', 'navy', 'olive', 'teal', 'violet', 'indigo', 'gold', 'silver', 'bronze', 'coral', 'crimson', 'fuchsia', 'khaki', 'lavender', 'salmon', 'tan', 'turquoise']
 
 const btn = document.getElementById('btn')
 const color = document.querySelector('.span-color')
@@ -8,7 +8,9 @@ btn.addEventListener('click', function(){
     const mainColor = document.querySelector('#main-color')
 
     mainColor.style.backgroundColor = colors[randomNumber]
-    color.textContent = colors[randomNumber]
+    
+    let texto = colors[randomNumber]
+    color.textContent = texto.charAt(0).toUpperCase() + texto.slice(1)
 })
 
 function getRandomNumber(){
